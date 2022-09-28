53887c72041d9a8621090a26efbbf117f2b98e0c
Kevin Getz <kgetz32@gmail.com>
2022-09-28 12:10:07 -0700

    created tabs and files for menu and contact

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import * as module from './homepage.js';
+import { initializeWebsite } from './homepage.js';
+import { menu } from './menu.js';
+import { contact } from './contact.js';
 
-console.log("Hello");
-module.initializeWebsite();
+initializeWebsite();
+
+let switchTabModule = (function() {
+
+    const homeTab = document.querySelector('.home');
+    homeTab.addEventListener(click, initializeWebsite);
+
+    const menuTab = document.querySelector('.menu');
+    menuTab.addEventListener(click, menu);
+
+    const contactTab = document.querySelector('.contact');
+    contactTab.addEventListener(click, contact);
+
+})();
